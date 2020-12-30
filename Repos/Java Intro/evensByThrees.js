cb9909c3815736c4f1b085196a2f1b64909943f0
NolanBD <75336475+NolanBD@users.noreply.github.com>
2020-12-29 19:28:14 -0500

    Add files via upload

---
+++ Repos/Java Intro/evensByThrees.js
@@ -0,0 +1,16 @@
+var num1 = 0;
+var num2 = 0;
+var num3 = 0;
+
+function clearErrors() {    
+    for (var loopCounter = 0; 
+        loopCounter < document.forms["numberFun"].elements.length; 
+        loopCounter++) {
+        if (document.forms["numberFun"].elements[loopCounter]
+           .parentElement.className.indexOf("has-") != -1) {
+            
+            document.forms["numberFun"].elements[loopCounter]
+               .parentElement.className = "form-group";
+        }
+    }
+}    
